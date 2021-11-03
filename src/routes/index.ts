648f1eec9472596bb57e06b47d45bba7da5af429
georginm <georginmatheus@gmail.com>
2021-11-03 08:42:24 -0300

    first commit

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+// Importa arquivos de rotas
+// import { categoriesRoutes } from './categories.routes';
+
+const router = Router();
+
+// Usa arquivo de rotas
+// router.use('/categories', categoriesRoutes);
+
+export { router };
